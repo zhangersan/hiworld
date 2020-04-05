a7e10679cbcf9a9a2c2262c9c248d69bb6ac002d
ersan <aideqing20@foxmail.com>
2020-04-05 16:21:52 +0800

    test

---
+++ src/HiWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @author 张慧铭
+ * @version V1.0
+ * @Package PACKAGE_NAME
+ * @date 2020/4/5 16:19
+ * @Copyright © 旺财有限公司
+ */
+public class HiWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi World!!");
+    }
+}
